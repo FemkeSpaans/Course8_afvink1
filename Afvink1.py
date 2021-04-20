a85fb45f45a7af0ada9253531a074a3de95e7df7
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2021-04-20 21:11:50 +0200

    Add files via upload

--- Afvink1.py
+++ Afvink1.py
@@ -3,35 +3,26 @@
 # Name: Afvink 1
 # Version: 1
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import mysql.connector
-
 
 app = Flask(__name__)
 
-@app.route('/')
-def database_connection():
-    conn = mysql.connector.connect(
-        host="ensembldb.ensembl.org",
-        user="anonymous",
-        database="homo_sapiens_core_91_38",
-        password="")
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT description FROM gene;")
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return rows
 
-@app.route('/', methods=["POST","GET"])
-def get_searchword():
-    found_searchword = ""
-    searchword = ""
+@app.route('/', methods=["GET", "POST"])
+def home():
+    bold_searchword=None
     if request.method == "POST":
-        searchword = request.form.get("searchword")
-        print (searchword)
-        found_searchword = filter_searchword(searchword)
+        searchword = get_searchword()
+        rows = filter_searchword(searchword)
+        bold_searchword = bold(rows, searchword)
+    return render_template("afvink1.html", rows=bold_searchword)
+
+
+def get_searchword():
+    searchword = request.form.get("searchword")
+    return searchword
+
 
 def filter_searchword(searchword):
     conn = mysql.connector.connect(
@@ -42,11 +33,20 @@ def filter_searchword(searchword):
     cursor = conn.cursor()
     searchword = searchword.replace("#", "").replace("\'", "")
     cursor.execute(
-        "SELECT description FROM gene WHERE description LIKE '%" + search_word + "%'")
+        "SELECT description FROM gene WHERE description LIKE '%" + searchword + "%'")
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
     return rows
 
+def bold(rows, searchword):
+    bold_searchword = []
+    for row in rows:
+        bold_result = str(row[0]).replace(searchword, fr"<b>{searchword}</b>")
+        bold_searchword.append(bold_result)
+    return bold_searchword
+
+
 if __name__ == '__main__':
     app.run()
+
